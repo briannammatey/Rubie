544f6245540c1acb93636eebe6208f7c073c2072
Brianna Matey <briianna@bu.edu>
2025-08-18 17:04:11 -0400

    working splash screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,22 @@
+/*
+
 import { Stack } from 'expo-router'
+import {useCallback} from 'react'
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync();
+
 
 const Layout = () =>{
+    const[fontsLoaded] = useFonts({
+        DMBOLD:require('../assets')
+
+    })
     return < Stack />;
 
 }
 
 export default Layout;
+
+*/
--- app/index.js
+++ app/index.js
@@ -1,58 +1,71 @@
 import React from 'react'
-import {View, Text, Image, ScrollView, TextInput} from 'react-native'
+import {View, Text, Image, ScrollView, TextInput, StyleSheet, Butto, TouchableOpacity} from 'react-native'
+import { createStaticNavigation } from '@react-navigation/native';
 
-const App = () =>{
-
-    return(
-        <View>
-            
-            <Image
-            source ={require( '../assets/images/clearimgrubie.png')}
-            style ={{width: 400, height:500,
-                justifyContent: 'center',
-                alignItems:'center'
-
-            }}
-            />
-        </View>
-    );
-};
-
-export default App;
-
-
-/*import {useState} from 'react'
-
-import{ View, Text } from 'react-native';
-
-import{Stack, useRouter} from 'expo-router'
-import {COLORS, icons, images, SIZES} from '../constants';
-import{Nearbyjobs, Popularjiobs, ScreenHeaderBtn, Welcome} from '../componsents'
-const Home = () => {
-    const router = useRouter();
-    return(
-        // save area view allows us to shows the constant safely without notcher
-        <SafeAreaView style = {{flex : 1, backgroundColor: COLORS.lightWhite}}>
-
-           <Stack.Screen
-            options ={{
-                headerStyle:{background:Colors.lightWhite},
-                headerShadowVisible: false,
-                headerLeft:() =>(
-                    <ScreenHeaderBtn iconUrl ={icons.menu} dimension = "60%" />
-                ),headerRight:()=>(<ScreenHeaderBtn iconUrl = {icons.profile} dimension = "100%"/>),
-                headerTitle: ""
-            }}
-
-           />
-        <ScrollView showsVerticalScrollIndicator = {false}></ScrollView>
-        </SafeAreaView>
-
-    )
-
-
+const App = ({onGetStarted}) =>{
+const handlePress =()=>{
+    onGetStarted()
 }
 
+   return(
+       <View style ={styles.container}>
+          
+           <Image
+           source ={require( '../assets/images/clearimgrubie.png')}
+           style ={{width: 300, height:400,
+               justifyContent: 'center',
+               alignItems:'center'
 
-export default Home;
-*/
+           }}
+           />
+           <Text style ={styles.appTitle}> Your PCOS Journey awaits </Text>
+           
+       <View style ={styles.buttonSection}>
+        <TouchableOpacity onPress ={handlePress} activeOpacity = {0.8}>
+        <Text style ={styles.buttonText}>Press to Begin</Text>
+   </TouchableOpacity>
+       </View>
+       
+       </View>
+   );
+   
+};
+
+const styles = StyleSheet.create({
+
+    container:{
+        flex:1,
+        backgroundColor:"#f4e0e8ff",
+        justifyContent: "space-between",
+        paddingVertical:60,
+        paddingHorizontal:50,
+    },
+    appTitle:{
+     fontSize : 28,
+     fontWeight: 'bold',
+     color : 'fff',
+     textAlign:'center',
+    paddingVertical:20
+     
+    },
+    buttonSection:{
+        backgroundColor:"#6b146bff",
+        paddingHorizontal:20,
+        borderRadius:60,
+        width:300,
+        height:80,
+        alignItems:'center'
+    },
+    buttonText:{
+        color:"white",
+        fontWeight:'bold',
+        textAlign: 'center',
+
+    paddingVertical:25,
+    fontSize:20
+
+
+    }
+})
+
+export default App;
